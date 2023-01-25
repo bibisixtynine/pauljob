b2033777c5cf8eaed5ca0d30097bb5ec8e8629ca
bibisixtynine <ilboued@me.com>
2023-01-25 22:38:41 +0100

    ok now from Codeflow :-)

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,43 @@
 import './styles.css';
+import Head from 'next/head';
+
+// viewport meta tag must be in _app.js (https://nextjs.org/docs/messages/no-document-viewport-meta)
 
 function App({ Component, pageProps }) {
+  <Head>
+<meta property="og:title" content="Générateur IA de Lettre de Motivation" key="title" />
+        <meta property="og:description" content="construit avec passion" key="description" />
+        <meta
+          property="og:image"
+          content="https://upload.wikimedia.org/wikipedia/commons/f/f1/CoffeeMath.png"
+        />
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <link
+          id="favicon"
+          rel="icon"
+          href="https://cdn.glitch.com/fd9e84f4-4370-493a-847e-22cebb631384%2Fbabyduck.webp?v=1628793057158"
+          type="image/x-icon"
+        />
+        <meta
+          name="viewport"
+          content="viewport-fit=cover,user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        {/*<meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />*/}
+        <link
+          rel="apple-touch-icon"
+          href="https://cdn.glitch.com/fd9e84f4-4370-493a-847e-22cebb631384%2Fbabyduck.webp?v=1628793057158"
+        />
+  </Head>
   return <Component {...pageProps} />
 }
 export default App;
+
+
+
+
+
+
